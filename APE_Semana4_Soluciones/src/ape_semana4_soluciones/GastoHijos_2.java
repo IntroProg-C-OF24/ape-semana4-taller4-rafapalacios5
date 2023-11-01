439fd7a5dfb6ee661f244ee7d6dcb6750c8a4d8c
Rafaella Palacios <repalacio5@utpl.edu.ec>
2023-11-01 16:44:28 -0500

    Ejercicio02

---
+++ APE_Semana4_Soluciones/src/ape_semana4_soluciones/GastoHijos_2.java
@@ -0,0 +1,19 @@
+
+package ape_semana4_soluciones;
+import java.util.Scanner;
+public class GastoHijos_2 {
+    public static void main(String[] args) {
+        double gastosHijo1, gastosHijo2, gastosHijo3, gastosTotales;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa los gastos del hijo 1:");
+        gastosHijo1 = teclado.nextDouble();
+        System.out.println("Ingresa los gastos del hijo 2:");
+        gastosHijo2 = teclado.nextDouble();
+        System.out.println("Ingresa los gastos del hijo 3:");
+        gastosHijo3 = teclado.nextDouble();
+        gastosTotales = gastosHijo1 + gastosHijo2 + gastosHijo3;
+        System.out.println("LOS GASTOS TOTALES DE LOS 3 HIJOS SON: " + gastosTotales);
+    }
+ 
+    
+}

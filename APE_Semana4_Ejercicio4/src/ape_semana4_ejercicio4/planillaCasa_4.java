098506ee869f8bf8869a7c9ad94ca9d632b22f3c
Rafaella Palacios <repalacios5@utpl.edu.ec>
2023-11-05 14:59:37 -0500

    Ejercicio 4

---
+++ APE_Semana4_Ejercicio4/src/ape_semana4_ejercicio4/planillaCasa_4.java
@@ -0,0 +1,19 @@
+/*
+Generar un algoritmo que permita calcular y mostrar el valor de la planilla de teléfono de un casa. 
+Se debe ingresar el costo por minutos, el número de minutos consumidos en el mes.
+*/
+package ape_semana4_ejercicio4;
+import java.util.Scanner;
+public class planillaCasa_4 {
+    public static void main(String[] args) {
+     double costoMinuto, minutosMes, valorPlanilla;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa el costo por minuto:");
+        costoMinuto = teclado.nextDouble();
+        System.out.println("Ingresa número de minutos consumidos en el mes:");
+        minutosMes = teclado.nextDouble();
+        valorPlanilla = costoMinuto * minutosMes;
+        System.out.println("EL VALOR DE LA PLANILLA ES: " + valorPlanilla);
+    }
+    
+}
